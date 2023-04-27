2dedda6f8cd78f6b38ea50bbbe51c2f4871511ae
divinirakiza <irakizadivin@gmail.com>
2023-04-27 17:12:26 +0200

    feat: setup react-query

---
+++ src/pages/users/page.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from 'react-query';
+
+import { fetchUserData } from '@/services/api.service';
+
+export default function ReactQueryTest() {
+  const { isLoading, error, data } = useQuery(`fetch-users`, () =>
+    fetchUserData()
+  );
+  return (
+    <div>
+      <h3>User data</h3>
+
+      <p>{!isLoading ? data.first_name + data.last_name : null}</p>
+    </div>
+  );
+}
