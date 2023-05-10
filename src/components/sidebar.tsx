c78c874b48106a81497d19dd830c178af885162f
gershomlapaix <nsengiyumv33@gmail.com>
2023-05-10 09:23:05 +0200

    feat: remove unused codes

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import React, { useState } from 'react';
 import { FaHome, FaHotel, FaRegistered } from 'react-icons/fa';
 import { MdSettings } from 'react-icons/md';
@@ -42,34 +41,6 @@ const Sidebar = () => {
   return (
     <div className="h-screen bg-[#FCDDEC] p-4 sm:p-[2rem]">
       <Logo withText={false} />
-      {/* <ul className="mt-10 flex flex-col justify-center space-y-8 sm:space-y-4">
-        <Link href={`/accomodation`} className="hover:no-underline">
-          <li className="flex items-center space-x-3 text-red-default">
-            <FaHome className="text-xl sm:text-base" />
-            <span className="hidden sm:block">Home</span>
-          </li>
-        </Link>
-        <Link href={`/accomodation/list`}>
-          <li className="flex items-center space-x-3 text-red-default">
-            <FaHotel className="text-xl sm:text-base" />
-            <span className="hidden sm:block">Accomodations</span>
-          </li>
-        </Link>
-
-        <Link href={`/accomodation/registration`}>
-          <li className="flex items-center space-x-3 px-1 py-2  text-lg text-red-default visited:bg-red-default active:bg-red-default active:text-white sm:px-2 sm:py-4">
-            <FaRegistered className="text-xl sm:text-base" />
-            <span className="hidden sm:block"> Register accomodation</span>
-          </li>
-        </Link>
-
-        <Link href={`/accomodation/settings`}>
-          <li className="flex items-center space-x-3 text-red-default">
-            <MdSettings className="text-2xl sm:text-base" />
-            <span className="hidden sm:block">Settings</span>
-          </li>
-        </Link>
-      </ul> */}
 
       <div className="my-10 flex flex-col justify-between gap-2">
         {tabs.map((tab, index) => {
